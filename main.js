5da5670c0d222b6e50a869ae4560160a98a930ef
moongeon <35988564+moongeon@users.noreply.github.com>
2020-09-26 01:27:50 +0900

    JS수정(스크롤 내릴시 홈화면 사라지게 만들기)

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const navbar = document.querySelector("#navbar");
 
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
-  console.log("y좌표 :",window.scrollY);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("narbar--dark");
   } else {
@@ -13,27 +12,33 @@ document.addEventListener("scroll", () => {
   }
 });
 
+// 홈화면에서 스크롤을 내릴때 점점 화면을 옅어지게 하면서 내리기
+
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 const navbar__menu = document.querySelector(".navbar__menu");
-navbar__menu.addEventListener('click',(evnet) => {
-console.log(event.target.dataset.link);
+navbar__menu.addEventListener("click", (evnet) => {
+  console.log(event.target.dataset.link);
 
- switch(event.target.dataset.link){
-   case '#home':
-     window.scrollTo(0,0);
-   break;
-   case '#about':
-     window.scrollTo(0,600);
-   break;
-   case '#skills':
-     window.scrollTo(0,1325);
-   break;
-   case '#mywork':
-     window.scrollTo(0,1800);
-   break;
-   case '#contact':
-     window.scrollTo(0,2200);
-   break;
- }
-
-
+  switch (event.target.dataset.link) {
+    case "#home":
+      window.scrollTo(0, 0);
+      break;
+    case "#about":
+      window.scrollTo(0, 600);
+      break;
+    case "#skills":
+      window.scrollTo(0, 1325);
+      break;
+    case "#mywork":
+      window.scrollTo(0, 1800);
+      break;
+    case "#contact":
+      window.scrollTo(0, 2200);
+      break;
+  }
 });
